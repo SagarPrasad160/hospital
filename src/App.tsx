0063e6ecaf29f6d3f71878ef9063187c22ae69da
SagarPrasad160 <sagarprasad916@gmail.com>
2023-08-11 14:38:07 +0530

    switch to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import SideBar from "./components/SideBar";
 import NavBar from "./components/NavBar";
